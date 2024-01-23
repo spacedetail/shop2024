629280d36b2ecc9f6ea8702bf45b00d9ef987a13
shopmall99 <serbible2021@gmail.com>
2024-01-23 19:23:27 +0900

    첫 커밋

---
+++ src/main/java/com/shop2024/Shop2024Application.java
@@ -0,0 +1,13 @@
+package com.shop2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop2024/Shop2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
